c22f64e586108681f891b401caaa30e887664d7f
CyanXshadow <bluexshadow6@gmail.com>
2023-10-15 00:21:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Task6 from "./components/Task6";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Task6 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
